fa5045b6c2f88eef5d5b08c50072fa2209c94d14
sachin <sachin.2.yadav@atos.net>
2020-10-09 14:12:27 +0530

    Updates

---
+++ precache-manifest.a233d8018ca31410419958a96f3ab9d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4ec0f2ca023c36821b18d7986264194",
+    "url": "/ANSIBLECLONE/index.html"
+  },
+  {
+    "revision": "69e376ddca9599407f55",
+    "url": "/ANSIBLECLONE/static/css/2.ea6b7726.chunk.css"
+  },
+  {
+    "revision": "042d1a54caa4c4e07bc6",
+    "url": "/ANSIBLECLONE/static/css/main.be3bcc70.chunk.css"
+  },
+  {
+    "revision": "69e376ddca9599407f55",
+    "url": "/ANSIBLECLONE/static/js/2.f09b04e8.chunk.js"
+  },
+  {
+    "revision": "e60f6644a238030ec5fb7683004cdede",
+    "url": "/ANSIBLECLONE/static/js/2.f09b04e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "042d1a54caa4c4e07bc6",
+    "url": "/ANSIBLECLONE/static/js/main.0512f113.chunk.js"
+  },
+  {
+    "revision": "bbb968a7c6bfb9fc9d75",
+    "url": "/ANSIBLECLONE/static/js/runtime-main.2c1322b4.js"
+  }
+]);
